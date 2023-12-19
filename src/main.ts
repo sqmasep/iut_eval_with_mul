c1064c754b8666ab5f962cff37dd6ef464b77791
sqmasep <leblond.romain@orange.fr>
2023-12-19 13:41:49 +0100

    [FEAT] Some features and animations

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import {
   animateMouseUp,
 } from "./utils/animations/animateMouse";
 import "./style.css";
-import { animate, scroll, spring } from "motion";
+import { animate, inView, scroll, spring, stagger, timeline } from "motion";
 import { revealText } from "./utils/animations/revealText";
 
 const buttons = document.querySelectorAll("button, a, .cursor-pointer");
@@ -63,6 +63,38 @@ scroll(
   }),
 );
 
+// scroll(
+//   animate(".neptune", {
+//     rotateZ: [0, -171],
+//     scale: [0.5, 0.8],
+//   }),
+//   {
+//     target: document.querySelector(".travel") as HTMLDivElement,
+//   },
+// );
+
+inView(".travel", ({ target }) => {
+  animate(
+    target.querySelector(".neptune")!,
+    {
+      scale: [1.0, 0.3],
+      x: [0, 580],
+      rotateZ: [0, -171],
+    },
+    { duration: 4 },
+  );
+
+  animate(
+    target.querySelector(".voyager")!,
+    {
+      x: [800, -780],
+      y: [0, 580],
+      rotateZ: [0, 120],
+    },
+    { duration: 4 },
+  );
+});
+
 document.addEventListener("DOMContentLoaded", () => {
   const video = document.querySelector(".video-intro") as HTMLVideoElement;
   video.addEventListener("play", e => {
@@ -75,4 +107,65 @@ document.addEventListener("DOMContentLoaded", () => {
       video.remove,
     );
   });
+});
+
+scroll(
+  animate(".fx-film", {
+    y: [-200, 200],
+    opacity: [0, 0.5],
+    scale: [1, 1.4],
+    rotateZ: [0, -10, 0],
+  }),
+  {
+    target: document.querySelector(".fx-film")?.parentElement!,
+    offset: ["start end", "end"],
+  },
+);
+
+inView(".urss, .usa", () => {
+  animate(
+    ".urss, .usa",
+    {
+      skewY: [0, 20, 0],
+      transformOrigin: ["bottom"],
+    },
+    { duration: 2, repeat: Infinity, easing: "linear" },
+  );
+});
+
+inView(".moon", () => {
+  animate(
+    ".moon",
+    {
+      rotateZ: [0, 360],
+    },
+    { duration: 30, repeat: Infinity, easing: "linear" },
+  );
+});
+
+inView(".yaytso", ({ target }) => {
+  timeline([
+    [
+      target.querySelector("h2")!,
+      {
+        clipPath: [
+          "polygon(0 0, 0 0, 0 100%, 0 100%)",
+          "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+        ],
+      },
+      { duration: 2 },
+    ],
+    [
+      target.querySelector("p")!,
+      {
+        clipPath: [
+          // from top to bottom
+          "polygon(0 0, 100% 0, 100% 0, 0 0)",
+          "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+        ],
+        opacity: [0, 1],
+      },
+      { duration: 1 },
+    ],
+  ]);
 });
